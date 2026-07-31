8ba6610206af61c59d7f7ebaa71b120575e950fe
Vivek Patel <vp@appkartz.com>
2026-07-31 11:22:54 -0600

    chore: run ftp test again without deploy

--- test-ftp.js
+++ test-ftp.js
@@ -2,48 +2,42 @@ import * as ftp from 'basic-ftp';
 
 async function run() {
     const client = new ftp.Client();
-    client.ftp.verbose = true;
     try {
         console.log("Connecting to FTP...");
         await client.access({
             host: process.env.FTP_SERVER,
             user: process.env.FTP_USERNAME,
             password: process.env.FTP_PASSWORD,
-            secure: false // Hostinger often prefers explicit TLS or plain FTP, basic-ftp will try plain if secure is false
+            secure: false
         });
         
-        console.log("--- CURRENT DIRECTORY ---");
+        console.log("--- CURRENT PWD ---");
         console.log(await client.pwd());
         
-        console.log("--- ROOT CONTENTS ---");
-        const listRoot = await client.list();
+        console.log("--- LISTING / ---");
+        const listRoot = await client.list("/");
         for (const item of listRoot) {
             console.log(item.type === 2 ? '[DIR] ' : '[FILE] ', item.name);
         }
 
-        console.log("--- CHECKING FOR PUBLIC_HTML ---");
-        if (listRoot.some(item => item.name === 'public_html')) {
-            const listPublic = await client.list('public_html');
-            console.log("--- PUBLIC_HTML CONTENTS ---");
-            for (const item of listPublic) {
-                console.log(item.type === 2 ? '[DIR] ' : '[FILE] ', item.name);
-            }
-        }
-        
-        console.log("--- CHECKING FOR DOMAINS ---");
-        if (listRoot.some(item => item.name === 'domains')) {
-            const listDomains = await client.list('domains');
-            console.log("--- DOMAINS CONTENTS ---");
+        if (listRoot.some(i => i.name === 'domains')) {
+            console.log("--- LISTING /domains ---");
+            const listDomains = await client.list("/domains");
             for (const item of listDomains) {
                 console.log(item.type === 2 ? '[DIR] ' : '[FILE] ', item.name);
             }
+            if (listDomains.some(i => i.name === 'firstpathdaycare.ca')) {
+                console.log("--- LISTING /domains/firstpathdaycare.ca ---");
+                const listFpdc = await client.list("/domains/firstpathdaycare.ca");
+                for (const item of listFpdc) {
+                    console.log(item.type === 2 ? '[DIR] ' : '[FILE] ', item.name);
+                }
+            }
         }
-
     }
     catch(err) {
         console.error("FTP Error:", err);
     }
     client.close();
 }
-
 run();
